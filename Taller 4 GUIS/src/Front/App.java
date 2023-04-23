5fb6c670e385072da9f4a6912783eb1f30f3409f
maria366p <111391599+maria366p@users.noreply.github.com>
2023-04-22 23:52:59 -0500

    Update App.java

--- Taller 4 GUIS/src/Front/App.java
+++ Taller 4 GUIS/src/Front/App.java
@@ -1,16 +1,11 @@
-package Front;
-
-import java.awt.BorderLayout;
 import java.io.IOException;
 
-import javax.swing.JLabel;
 
 public class App {
 	public static void main(String[] args) throws IOException
     {
-		MainFrame tablero= new MainFrame();
+		FramePrincipal tablero= new FramePrincipal();
 		
 		
     }
 }
-
